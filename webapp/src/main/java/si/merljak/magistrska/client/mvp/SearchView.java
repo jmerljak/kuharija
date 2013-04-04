5daa0f93d816105f829643d17f58978896e49628
Jakob Merljak <jakob.merljak@student.uni-lj.si>
2013-04-04 21:12:33 +0200

    fix: widgets with handlers cannot be added directly to root panel used to init (view extending) composite

--- webapp/src/main/java/si/merljak/magistrska/client/mvp/SearchView.java
+++ webapp/src/main/java/si/merljak/magistrska/client/mvp/SearchView.java
@@ -8,10 +8,10 @@ import si.merljak.magistrska.common.dto.RecipeDto;
 import com.github.gwtbootstrap.client.ui.Heading;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.event.dom.client.KeyCodes;
 import com.google.gwt.event.dom.client.KeyUpEvent;
 import com.google.gwt.event.dom.client.KeyUpHandler;
 import com.google.gwt.user.client.History;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
@@ -29,35 +29,45 @@ public class SearchView extends AbstractView {
 	private FlowPanel resultsPanel = new FlowPanel();
 
 	public SearchView () {
+		main.add(new Heading(2, constants.search()));
+		main.add(resultsPanel);
+		initWidget(main);
+
+		RootPanel formPanel = RootPanel.get("searchField");
+		formPanel.setStyleName("input-append");
+		formPanel.add(searchBox);
+		formPanel.add(searchButton);
+
+		searchBox.getElement().setId("appendedInputButtons");
 		searchBox.addKeyUpHandler(new KeyUpHandler() {
 			@Override
 			public void onKeyUp(KeyUpEvent event) {
-				Window.alert("aaa");
-//				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-//					String searchString = searchBox.getValue().trim();
-//					History.newItem("search&q=" + searchString);
-//				}
+				if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
+					doSearch();
+				}
 			}
 		});
+		searchButton.setStyleName("btn");
 		searchButton.addClickHandler(new ClickHandler() {
 			@Override
 			public void onClick(ClickEvent event) {
-				Window.alert("bbb");
-				String searchString = searchBox.getValue().trim();
-				History.newItem("search&q=" + searchString);
+				doSearch();
 			}
 		});
+	}
 
-		main.add(new Heading(2, constants.search()));
-		main.add(searchBox);
-		main.add(searchButton);
-		main.add(resultsPanel);
-		initWidget(main);
+	private void doSearch() {
+		String searchString = searchBox.getValue().trim();
+		History.newItem("search&q=" + searchString);
 	}
 
 	public void displaySearchResults(List<RecipeDto> results, SearchParameters parameters) {
-		searchBox.setText(parameters.getSearchString());
+		// clear old data
 		resultsPanel.clear();
+
+		// display search string, filters, sorting
+		searchBox.setText(parameters.getSearchString());
+		// TODO filters, sorting
 
 		if (results.isEmpty()) {
 			resultsPanel.add(new Label(constants.searchNoResults()));
@@ -74,6 +84,7 @@ public class SearchView extends AbstractView {
 			recipe.add(image);
 			recipe.add(new Label(localizeEnum(result.getDifficulty())));
 			recipe.add(new Label(timeFromMinutes(result.getTimeOverall())));
+
 			resultsPanel.add(recipe);
 		}
 		setVisible(true);
